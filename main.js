59619d8e808b10011e6936cd30cb474618b99789
Eddy Brown <eddybrown@me.com>
2020-08-21 08:46:54 +0100

    Set window width

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const { app, BrowserWindow } = require('electron');
 function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 780,
+    width: 630,
+    height: 790,
     webPreferences: {
       nodeIntegration: true,
     },
